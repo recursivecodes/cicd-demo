9a4065af31ff1e19db3e6cf5c1181a16fea7b43d
recursivecodes <toddraymondsharp@gmail.com>
2021-10-19 11:17:55 -0400

    add liquibase, domain, repository, controller methods and tests for User persistence

--- src/test/groovy/codes/recursive/AbstractSpec.groovy
+++ src/test/groovy/codes/recursive/AbstractSpec.groovy
@@ -1,12 +1,6 @@
 package codes.recursive
 
 import io.micronaut.context.ApplicationContext
-import io.micronaut.context.env.Environment
-import io.micronaut.data.model.query.builder.sql.Dialect
-import io.micronaut.data.runtime.config.SchemaGenerate
-import org.testcontainers.containers.BindMode
-import org.testcontainers.containers.OracleContainer
-import org.testcontainers.containers.wait.strategy.Wait
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
@@ -15,23 +9,9 @@ class AbstractSpec extends Specification {
     @Shared
     @AutoCleanup
     static ApplicationContext context
-    static {
-        OracleContainer oracleContainer = new OracleContainer("gvenzl/oracle-xe:18-slim")
-        oracleContainer.start()
 
-        System.setProperty("oracle.jdbc.fanEnabled", "false")
-
-        context = ApplicationContext.run(
-                [
-                        "datasources.default.connection-factory-class-name": "oracle.jdbc.pool.OracleDataSource",
-                        "datasources.default.driver-class-name": "oracle.jdbc.driver.OracleDriver",
-                        "datasources.default.url": oracleContainer.getJdbcUrl(),
-                        "datasources.default.username": oracleContainer.getUsername(),
-                        "datasources.default.password": oracleContainer.getPassword(),
-                        "datasources.default.schema-generate": SchemaGenerate.NONE,
-                        "datasources.default.dialect": Dialect.ORACLE
-                ],
-                Environment.TEST
-        )
+    static  {
+        context = ApplicationContext.run()
     }
+
 }
