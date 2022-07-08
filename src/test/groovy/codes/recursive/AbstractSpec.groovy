95329676c9696f6fc37db36b192676d3a411055b
Todd Sharp <41924046+recursivecodes@users.noreply.github.com>
2022-07-08 12:18:15 -0400

    Update AbstractSpec
    @MicronautTest will initialize the application context, so removing it from abstract spec.

--- src/test/groovy/codes/recursive/AbstractSpec.groovy
+++ src/test/groovy/codes/recursive/AbstractSpec.groovy
@@ -6,12 +6,5 @@ import spock.lang.Shared
 import spock.lang.Specification
 
 class AbstractSpec extends Specification {
-    @Shared
-    @AutoCleanup
-    static ApplicationContext context
-
-    static  {
-        context = ApplicationContext.run()
-    }
 
 }
